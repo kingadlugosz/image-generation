a1013b98cc495dd012edc169fd74154efbaa351c
Kinga Długosz <kinga.dlugosz@nowagazeta.pl>
2023-08-06 19:00:34 +0200

    added dataloader

--- src/data_preparation/load_data.py
+++ src/data_preparation/load_data.py
@@ -2,12 +2,23 @@ import torch
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import hydra
+from hydra.utils import instantiate
+from torch.utils.data import DataLoader
 
 
 @hydra.main(version_base="1.3", config_path="../../conf", config_name="config")
 def load_data(cfg):
-    dataset = datasets.ImageFolder(cfg.paths.dataset + cfg.dataset.name)
-    dataset[476][0].show()
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((64, 64)),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+
+    dataset = datasets.ImageFolder(cfg.paths.dataset + cfg.dataset.name, transform=transform)
+
+    dataloader = DataLoader(dataset, **cfg.dataset.data_loader)
+
+    return dataloader
 
 
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from src.data_preparation.load_data import load_data
 
 @hydra.main(version_base="1.3", config_path="../conf", config_name="config")
 def main(cfg: DictConfig) -> None:
-    load_data(cfg)
+    dataloader = load_data(cfg)
 
 
 if __name__ == "__main__":
