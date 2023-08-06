f0f25a2fbaf41f7cca3b66d02ecb01a173346dc7
Kinga Długosz <kinga.dlugosz@nowagazeta.pl>
2023-08-06 19:34:09 +0200

    created dcgan

---
+++ src/model_preparation/dcgan.py
@@ -0,0 +1,16 @@
+class DCGan:
+    def __init__(self):
+        self.generator = self.create_generator()
+        self.discriminator = self.create_discriminator()
+
+    def create_generator(self):
+        return None
+
+    def create_discriminator(self):
+        return None
+
+    def train_generator(self):
+        pass
+
+    def train_discriminator(self):
+        pass
