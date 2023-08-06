fac34ac3367c6bc7984ba3516eb10e884ff62ce6
Kinga Długosz <kinga.dlugosz@nowagazeta.pl>
2023-08-06 19:33:59 +0200

    created load model

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 from src.data_preparation.load_data import load_data
+from src.model_preparation.load_model import load_model
 
 
 @hydra.main(version_base="1.3", config_path="../conf", config_name="config")
 def main(cfg: DictConfig) -> None:
     dataloader = load_data(cfg)
+    model = load_model(cfg)
 
 
 if __name__ == "__main__":
---
+++ src/model_preparation/load_model.py
@@ -0,0 +1,13 @@
+import hydra
+from src.model_preparation.dcgan import DCGan
+
+
+
+@hydra.main(version_base="1.3", config_path="../../conf", config_name="config")
+def load_model(cfg):
+    model = DCGan()
+    return model
+
+
+if __name__ == "__main__":
+    load_model()
